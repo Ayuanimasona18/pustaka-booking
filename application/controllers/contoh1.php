1c1dbd3fa85a456bd55302f4c82d2dd3060efacc
Ayuanimasona18 <ayuanimasona065@gmail.com>
2023-09-23 11:24:46 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>" ;
+        echo "Nama saya Ayu Animasona
+             Saya tinggal di daerah Pati
+             olah raga yang saya sukai adalah
+             Bulutangkis";
+    }
+}
